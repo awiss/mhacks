c6966a989a1d33d872aaea444d95d36602010a6d
Alex Wissmann <awiss@wharton.upenn.edu>
2013-09-21 14:04:00 -0400

    Fine bitch

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,6 +13,7 @@ exports.model = function(req,res){
 	var modelName = req.params.model;
 	modelName = modelName.replace('_',' ');
 	Article.find({ name: modelName, type: 'model'}, function(err, docs){
+		res.send(docs);
 		var values = [];
 		var dates = [];
 		for(var i = 0; i < docs.length; i++) {
@@ -26,12 +27,13 @@ exports.model = function(req,res){
 exports.make = function(req,res){
 	var makeName = req.params.make;
 	makeName = makeName.replace('_', ' ');
-	Article.find({ name: makeName, type: 'make'}, function(err, docs){
+	Article.find({ name: makeName, type: 'make'}).sort({dateInt:'asc'}).exec(function(err, docs){
 		var values = [];
 		var dates = [];
 		for(var i = 0; i < docs.length; i++) {
 			values.push(docs[i].sentimentValue * docs[i].relevance);
 			dates.push(docs[i].dateInt);
 		}
+		res.render('index', {modelName: makeName, values: values, dates: dates});
 	});
 };
