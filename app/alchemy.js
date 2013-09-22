814b7881b2cbd84ea60698f0e9a14800753288a1
Spencer Moran <smoran02@gmail.com>
2013-09-22 02:31:21 -0700

    add article database

--- app/alchemy.js
+++ app/alchemy.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 require('./models/entity')(mongoose);
+require('./models/article')(mongoose);
 var Entity = mongoose.model("Entity");
+var Article = mongoose.model("Article");
 var AlchemyAPI = require('alchemy-api');
 var alchemy = new AlchemyAPI('2094dd01fd7cbceb7e1bb916840e40e81f25d16f');
 
@@ -69,10 +71,10 @@ function processArticles(hearst_response){
     var article = articles[i];
     var text = article.bodyHTML.body.replace(/<.*?>/g,"");
     (function(theArticle){
-      alchemy.entities(text, {sentiment:1}, function(err, response) {
+      alchemy.keywords(text, {sentiment:1}, function(err, response) {
         if (!err){
         // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
-          var entities = response.entities;
+          var entities = response.keywords;
           for (var j=0; j<entities.length;j++) {
             var entity = entities[j];
             var type = "make";
@@ -90,7 +92,7 @@ function processArticles(hearst_response){
               }
               var name = match.text;
               name = name.charAt(0).toUpperCase() + name.slice(1);
-              Entity.create({type:type, sentimentType:match.sentiment.type, dateInt:new Date(theArticle.publishDate).getTime(),
+              Article.create({type:type, sentimentType:match.sentiment.type, dateInt:new Date(theArticle.publishDate).getTime(),
                 relevance:match.relevance,sentimentValue:score,name:name,title:theArticle.fullTitle,body:theArticle.bodyHTML.body}, 
                 function(error){
                   console.log(error);
